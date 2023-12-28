5980eda6081cd88604b97bc40e622f7ff102a570
Ais-Danny <2843508990@qq.com>
2023-12-28 14:33:15 +0800

    new

--- diy1.sh
+++ diy1.sh
@@ -1,1 +1,4 @@
 #sed -i '1i src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git' feeds.conf.default
+
+# 修改默认网关IP
+sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
